1368ea8ca58bfb5820cd4d1742444a5b08912c3e
Fifaliana <hei.fifaliana@gmail.com>
2024-03-22 22:50:14 +0300

    feat: server rendering

---
+++ src/app/server/page.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+import ServerComponent from "@/components/pokemon/PokemonServer";
+
+interface Pokemon {
+    name: string;
+    url: string;
+}
+
+let pokemonData: Pokemon[] = [];
+
+const fetchData = async () => {
+    try {
+        const response = await fetch(`https://pokeapi.co/api/v2/pokemon`);
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        const data = await response.json();
+        pokemonData = data.results;
+        render();
+    } catch (error) {
+        console.error('Error fetching data:', error);
+        pokemonData = [];
+        render();
+    }
+};
+
+const render = () => {
+    forceUpdatePage && forceUpdatePage();
+};
+let forceUpdatePage: (() => void) | null = null;
+export const setForceUpdatePage = (update: () => void) => {
+    forceUpdatePage = update;
+};
+
+fetchData();
+
+export default function Page() {
+    return (
+        <div>
+            <ServerComponent pokemonData={pokemonData} />
+        </div>
+    );
+}
---
+++ src/components/pokemon/PokemonServer.tsx
@@ -0,0 +1,58 @@
+import React from 'react';
+
+interface Props {
+    pokemonData: {
+        name: string;
+        url: string;
+    }[];
+}
+
+export default function ServerComponent(props: Props) {
+    const { pokemonData } = props;
+
+    return (
+        <div className="flex flex-wrap justify-center gap-6">
+            {pokemonData.map(pokemon => (
+                <div key={pokemon.name} className="card w-96 bg-base-100 shadow-xl image-full">
+                    <img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${getIdFromUrl(pokemon.url)}.png`} alt={pokemon.name} />
+                    <div className="card-body">
+                        <h2 className="card-title">{pokemon.name}</h2>
+                        <p>Pokemon number: {getIdFromUrl(pokemon.url)} 🍳</p>
+                        <div className="card-actions justify-end">
+                            <button className="btn btn-primary">
+                                <a href="/server">Details</a>
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            ))}
+        </div>
+    );
+}
+
+export async function getServerSideProps() {
+    try {
+        const res = await fetch("https://pokeapi.co/api/v2/pokemon");
+        if (!res.ok) {
+            throw new Error("Failed to fetch data");
+        }
+        const jsonData = await res.json();
+        const pokemonData = jsonData.results;
+        return {
+            props: {
+                pokemonData
+            }
+        };
+    } catch (error) {
+        console.error('Error fetching data:', error);
+        return {
+            props: {
+                pokemonData: []
+            }
+        };
+    }
+}
+
+function getIdFromUrl(url: string) {
+    return url.split("/").slice(-2, -1)[0];
+}
